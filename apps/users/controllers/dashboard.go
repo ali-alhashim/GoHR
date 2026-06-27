beece6d59ce08399c47591dd2bc415f209078bfc
Ali Alhashim <ali-alhashim@outlook.com>
2026-06-27 19:30:30 +0300

    update user

--- apps/users/controllers/dashboard.go
+++ apps/users/controllers/dashboard.go
@@ -9,12 +9,13 @@ func Dashboard(w http.ResponseWriter, r *http.Request) {
 	 
 
 	
-    
+    var User = core.GetCurrentUser(r)
 
 	
 
 	data := map[string]interface{}{
 		"Title": "Dashboard",
+		"User":User,
 	
 	}
 
--- core/database_func.go
+++ core/database_func.go
@@ -166,7 +166,7 @@ func GetUserByEmail(email string) *usersModels.User {
 
 	 query := `
         SELECT 
-            u.id, u.name, u.email, u.active, u.online, u.last_login, u.created_at, u.updated_at, 
+            u.id, u.name, u.local_name,u.badge_id, u.email, u.active, u.online, u.last_login, u.created_at, u.updated_at, 
             r.id, r.name, r.description,
             p.id, p.resource, p.action
         FROM users u
@@ -204,7 +204,7 @@ func GetUserByEmail(email string) *usersModels.User {
         )
 
         err := rows.Scan(
-            &user.ID, &user.Name, &user.Email, &user.Active, &user.Online, &user.LastLogin, &user.CreatedAt, &user.UpdatedAt, 
+            &user.ID, &user.Name,&user.LocalName,&user.BadgeId, &user.Email, &user.Active, &user.Online, &user.LastLogin, &user.CreatedAt, &user.UpdatedAt, 
             &rID, &rName, &rDesc,
             &pID, &pResource, &pAction,
         )
